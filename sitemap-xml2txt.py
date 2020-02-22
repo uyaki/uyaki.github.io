084e3f5ffdd1e405353ecbc2e0e0b86c55542011
gknoone <737376326@qq.com>
2020-02-22 15:59:38 +0800

    rebuilding site 2020年 2月22日 星期六 15时59分38秒 CST

--- sitemap-xml2txt.py
+++ sitemap-xml2txt.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+# ！usr/bin/python
+# ==========================
+import xml.etree.ElementTree as ET
+
+print('  😜reading sitemap.xml...')
+# 读取当前目录下到sitemap.xml文件
+tree = ET.ElementTree(file='sitemap.xml')
+root = tree.getroot()
+# 写入到sitemap.txt
+print('    🤪write into sitemap.txt...')
+url_no = 0;
+f = open('./sitemap.txt', 'w')
+for url in root.findall('{http://www.sitemaps.org/schemas/sitemap/0.9}url'):
+    loc = url.find('{http://www.sitemaps.org/schemas/sitemap/0.9}loc')
+    url_no = url_no + 1
+    print('    🔍find url : ', url_no)
+    f.write(loc.text + "\n")
+f.close()
+print('  🥳successful!!!')
+
+# -*- coding: utf-8 -*-
 # ! usr/bin/python
 # ==========================
 import xml.etree.ElementTree as ET
